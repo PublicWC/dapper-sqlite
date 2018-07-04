4db9852d404411e7bd667cee223e108de51c86e4
Chris Wong <wc38.wong.public@gmail.com>
2018-07-04 15:05:16 +0800

    Dapper.SqlBuilder join

--- Program.cs
+++ Program.cs
@@ -4,58 +4,31 @@ using System;
 using System.Data.SQLite;
 using System.IO;
 
+
 namespace dapper_sqlite
 {
     class Program
     {
+         
+        static string dbPath = @".\orders.sqlite";
+        static string cnStr = "data source=" + dbPath;
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            InitSQLiteDb();
-            TestInsert();
-            TestSelect();
-        }
- 
-        static string dbPath = @".\Test.sqlite";
-        static string cnStr = "data source=" + dbPath;
- 
-        static void InitSQLiteDb()
-        {
-            if (File.Exists(dbPath)) return;
-            using (var cn = new SQLiteConnection(cnStr))
-            {
-                cn.Execute(@"
-CREATE TABLE Orders 
-(
-    OrderId         VARCHAR(16),
-    UserContactId   VARCHAR(16),
-    InsertDate      DATE,
-    CONSTRAINT Orders_PK PRIMARY KEY (OrderId)
-);");
-            }
-        }
- 
 
+            using (var cn = new SQLiteConnection(cnStr))
+            {
+                var builder = new SqlBuilder();
+                var template = builder.AddTemplate("Select * from [Orders] O ");
+                builder.Select("O.OrderId")
+                        .Join("Contacts UC ON UC.UserContactId = O.UserContactId");
 
-        static void TestInsert()
-        {
-            using (var cn = new SQLiteConnection(cnStr))
-            {
-                //參數是用@paramName
-                var insertScript = 
-                    "INSERT INTO Orders VALUES('1', 'B', '2018-06-19')";
-                cn.Execute(insertScript);
-            }
-        }
- 
-        static void TestSelect()
-        {
-            using (var cn = new SQLiteConnection(cnStr))
-            {
-                var list = cn.Query("SELECT * FROM Orders");
+                var list = cn.Query(template.RawSql);
                 Console.WriteLine(
                     JsonConvert.SerializeObject(list, Formatting.Indented));
             }
+
         }
     }
 }
